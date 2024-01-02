be139ad561a8818f9d0184b7693b458d1ca8acd7
EduAir94 <49965850+EduAir94@users.noreply.github.com>
2024-01-01 21:56:13 -0300

    feat: vercel redirection api

--- index.ts
+++ index.ts
@@ -2,12 +2,16 @@ import { Request, Response } from "express";
 import server from "./classes/Express/ExpressSetup";
 import { gemini } from "./classes/Gemini";
 async function main() {
+  console.log("Run server");
   server.postJson("api/json", async (req: Request, res: Response) => {
     const response = await gemini.generateContentReq(req.body.prompt);
     if (!response) {
       return { error: "No response", text: "" };
     }
     return { text: response };
+  });
+  server.getJson("api", async (req: Request, res: Response) => {
+    return { text: "Im alive" };
   });
 }
 
